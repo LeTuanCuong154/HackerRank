a8f3a72ad4bff067ac4b411beea57a4e1dd885b4
LeTuanCuong154 <letuancuong154@gmail.com>
2019-02-22 18:06:29 +0700

    FIX_database

---
+++ 30dayofcode/Day 6/Let's Review.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int t;
+    scanf("%d",&t);
+    while(t)
+        {
+        char a[10000];
+        scanf("%s",a);
+        int i=0;
+        while(a[i]!='\0')
+        {if(i%2==0)
+            printf("%c",a[i]);
+        i++;}
+       i=0;
+        printf(" ");
+        while(a[i]!='\0')
+        {if(i%2!=0)
+            printf("%c",a[i]);
+        i++;}
+        t--;
+        printf("\n");
+    }
+    
+    return 0;
+}
