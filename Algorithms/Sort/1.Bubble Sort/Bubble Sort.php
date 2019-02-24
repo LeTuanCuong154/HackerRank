d1761afe650c4b73f3cac8edd1713a72224e641f
LeTuanCuong154 <letuancuong154@gmail.com>
2019-02-24 17:48:02 +0700

    upgrade_sort

--- Algorithms/Sort/1.Bubble Sort/Bubble Sort.php
+++ Algorithms/Sort/1.Bubble Sort/Bubble Sort.php
@@ -1,4 +1,29 @@
 <?php
+    $numbers = [3, 8, 7, 2, 4, 5, 1, 9, 6,11,10]; 
+    $c = count($numbers);
+    for ($i=0;$i<$c;$i++)
+        {
+            echo  $numbers[$i]."|";
+        }
+    $tmp = 0;
+    $o = 0;
+    for($o;$o<count($numbers);$o++)   
+     {
+        for ($i = (count($numbers)-1);$i>$o;$i--)
+        {
+            if ($numbers[$i] < $numbers[$i-1])
+            { 
+                $tmp = $numbers[$i];
+                $numbers[$i] =  $numbers[$i-1];
+                $numbers[$i-1] = $tmp;
+            }
+       
+        }
+    }
+    echo "--------";
+    for ($i=0;$i<$c;$i++)
+        {
+            echo  $numbers[$i]."|";
+        }
 
-
-?> 
+?>
