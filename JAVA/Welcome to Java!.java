3faa016f5fc95a77c6847148ec0038b432dcfd08
LeTuanCuong154 <letuancuong154@gmail.com>
2019-03-10 10:57:29 +0700

    Java_Welcome_Loop

---
+++ JAVA/Welcome to Java!.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+public class Solution {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, World.");
+       System.out.println("Hello, Java.");
+    }
+}
+
